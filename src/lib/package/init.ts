f3791ddbd3808459b7a06c76b70fdafcb7bd5b9b
janslan <janko.slanina@hotmail.com>
2025-08-07 15:01:56 +0200

    Properly fixed versioning system

--- src/lib/package/init.ts
+++ src/lib/package/init.ts
@@ -4,6 +4,30 @@ import { logToConsole } from '../../lib/console/init';
 
 export class packageManager {
 	async searchForUpdates(): Promise<boolean> {
+		async function compareVersions(a: string, b: string): Promise<number> {
+			const previous = a.split('.').map(Number);
+			const current = b.split('.').map(Number);
+
+			let currentN: string = '';
+			let previousN: string = '';
+
+			for (const n of previous) {
+				previousN = previousN + n.toString();
+			}
+
+			for (const n of current) {
+				currentN = currentN + n.toString();
+			}
+
+			if (parseInt(previousN) > parseInt(currentN)) {
+				return -1;
+			} else if (parseInt(currentN) > parseInt(previousN)) {
+				return 1;
+			}
+
+			return 0;
+		}
+
 		logToConsole('Green', 'Searching for updates...');
 		const request = await axios({
 			url: 'https://api.github.com/repos/lunarprogramas/env-cleaner/releases/latest',
@@ -31,20 +55,15 @@ export class packageManager {
 			return false;
 		}
 
-		// run an arithmetic operation to determine if the version numbers truely are what they say
-		let versionNumber: string | number = latestVersionNo.replace('.', '');
-		versionNumber = parseInt(versionNumber);
+		const versionComparison: number = await compareVersions(packageVersion, latestVersionNo);
 
-		let currentVersionNumber: string | number = packageVersion.replace('.', '');
-		currentVersionNumber = parseInt(currentVersionNumber);
-
-		if (versionNumber > currentVersionNumber) {
+		if (versionComparison === 1) {
 			logToConsole(
 				'Yellow',
 				`There is an update available! (${packageVersion} -> ${latestVersionNo}) Run npm update env-cleaner to get the latest version.`
 			);
 			return true;
-		} else if (currentVersionNumber > versionNumber) {
+		} else if (versionComparison === -1) {
 			logToConsole(
 				'Yellow',
 				`This package is either deprecated or the github is seriously outdated.`
